2a0d4e51dce51ead7f9eb851bc1ba089b09723e9
diegogarcia9907 <dfgarcia43@ucatolica.edu.co>
2024-05-06 22:05:24 -0400

    se crea una clase de tipo logger que utiliza el patron singleton y se adecuara segun la historia de usuario

---
+++ src/main/java/com/example/Jardineria/Auditoria/Logger/LoggerManager.java
@@ -0,0 +1,22 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerManager {
+    private static final Logger logger = LoggerFactory.getLogger(LoggerManager.class);
+    private static LoggerManager instance;
+
+    private LoggerManager() {
+        // Constructor privado para prevenir instanciación directa
+    }
+
+    public static synchronized LoggerManager getInstance() {
+        if (instance == null) {
+            instance = new LoggerManager();
+        }
+        return instance;
+    }
+
+    public void logError(String message) {
+        logger.error(message);
+    }
+}
